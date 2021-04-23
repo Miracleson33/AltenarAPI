1b70dbe2f922ebc15835742e7abbafe5caa115df
Miracleson33 <michael.subbotin.82@gmail.com>
2021-04-23 04:58:23 +0300

    Test task from Altenar
    Test task from Altenar

---
+++ AltenarAPI/Db/DbServiceExtension.cs
@@ -0,0 +1,21 @@
+﻿using AltenarAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltenarAPI
+{
+    // Регистрация DbContext как службы
+    public static class DbServiceExtension
+    {
+        // Определение статического метода расширения AddDatabaseService, который отвечает за регистрацию DbContext, использующего поставщика базы данных SQL Server в контейнере DI.
+        public static void AddDatabaseService(this
+        IServiceCollection services, string connectionString)
+        => services.
+        AddDbContext<AltenarAPIContext>(options => options.
+        UseSqlServer(connectionString));
+    }
+}
---
+++ AltenarAPI/Db/Models/AltenarAPIContext.cs
@@ -0,0 +1,32 @@
+﻿using AltenarAPI.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltenarAPI.Models
+{
+    // Определение контекста базы данных AltenarAPIContext
+    public class AltenarAPIContext : DbContext
+    {
+        public AltenarAPIContext(DbContextOptions<AltenarAPIContext> options) : base(options) { }
+
+        // Определение сущности таблицы IncomingMessages DbSet для модели IncomingMessage
+        public DbSet<IncomingMessage> IncomingMessages { get; set; }
+
+        // Определение сущности таблицы Messages DbSet для модели Parameter
+        public DbSet<Parameter> Parameters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка отношения «один ко многим» между моделями таблиц IncomingMessages и Parameters
+            modelBuilder.Entity<IncomingMessage>()
+            .HasMany(p => p.Parameters)
+            .WithOne();
+
+            // Предварительная загрузка данных в БД
+            modelBuilder.Seed();
+        }
+    }
+}
